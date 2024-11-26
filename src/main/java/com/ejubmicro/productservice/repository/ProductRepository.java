bfbf4d5e8d64433024de8af4a22479a6f11ee4ee
Elekoumpo <emezit1@etf.unsa.ba>
2024-11-26 23:47:54 +0100

    Clean up unnecessary files

--- src/main/java/com/ejubmicro/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ejubmicro.productservice.repository;
-
-import com.ejubmicro.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String>{
-
-}
